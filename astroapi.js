5b693fe038131f259e2e664819af6d74691a65cf
Greg <greg@Gregs-MacBook-Pro.local>
2021-02-14 15:22:35 -0500

    using worldtimeapi.org for time and date

--- astroapi.js
+++ astroapi.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const router = express.Router();
+const fetch = require("node-fetch");
 
 var swisseph = require('swisseph');
 var assert = require('assert');
@@ -55,8 +56,8 @@ router.get('/planets', (req, res) => {
     res.json(planetaryPositions);
 });
 
-router.get('/date', (req, res) => {
-    var dateObject = new Date(Date.now());
+router.get('/serverDate', (req, res) => {
+    var dateObject = new Date(Date.now()); // server date and time, which is sometimes wrong on AWS
     var date = {year: 0, month: 0, day: 0, hour: 0};
     date["year"] = dateObject.getFullYear();
     date["month"] = (dateObject.getMonth() + 1); // add one because the object counts from zero and swisseph counts from one
@@ -67,45 +68,110 @@ router.get('/date', (req, res) => {
     res.send(("Year: " + date["year"] + " Month: " + date["month"] + " Day: " + date["day"] + " Hour: " + date["hour"] + " Value: " + julday));
 });
 
+router.get('/dateFromApi', (req, res) => {
+    var dateTimeString = "";
+    fetch('http://worldtimeapi.org/api/timezone/Etc/UTC', {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          // 'Content-Type': 'application/json'
+          'Content-Type': 'application/x-www-form-urlencoded', // this does not trigger a CORS preflight, works with AWS
+        },
+        // redirect: 'follow', // manual, *follow, error
+        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        // body: "" // body data type must match "Content-Type" header
+      })
+    .then(res => res.json())
+    .then(json => {
+        dateTimeString = new String(json["datetime"]);
+        console.log(dateTimeString);
+    })
+    .then(result => {
+        console.log('Success:', result);
+        // var dateObject = new Date(unixTime); // for some reason this isn't working -- may need to parse out date string and stick in JSON object below
+        var date = {year: 0, month: 0, day: 0, hour: 0};
+        const dateItems=dateTimeString.split("-");
+        console.log(dateItems);
+        for (i = 0; i < dateItems.length; i++) {
+            console.log(dateItems[i]);
+        }
+        date["year"] = parseInt(dateItems[0]);
+        date["month"] = parseInt(dateItems[1]); 
+        date["day"] = parseInt(dateItems[2]); 
+        var dateHours = (dateItems[2].slice((dateItems[2].indexOf("T") + 1), (dateItems[2].indexOf(".") - 1))); // read the hours between the T and the . non-inclusive
+        var dateHourItems = dateHours.split(":"); // split into strings for hours, minutes, seconds
+        date["hour"] = (parseInt(dateHourItems[0]) + (parseInt(dateHourItems[1]) / 60) + (parseInt(dateHourItems[2]) / 3600));
+        var julday = swisseph.swe_julday(date.year, date.month, date.day, date.hour, swisseph.SE_GREG_CAL);
+        // res.json(julday);
+        res.send(("Year: " + date["year"] + " Month: " + date["month"] + " Day: " + date["day"] + " Hour: " + date["hour"] + " Value: " + julday));
+      })
+    .catch(error => {
+        console.error('Error:', error);
+        res.send("API error");
+      });
+});
+
 router.get('/siderealPlanets', (req, res) => {
     // path to ephemeris data
     swisseph.swe_set_ephe_path (__dirname + '/../ephe');
     // set ayanamsa to standard Lahiri
     swisseph.swe_set_sid_mode(swisseph.SE_SIDM_LAHIRI, 0, 0);
     var flag = swisseph.SEFLG_SIDEREAL | swisseph.SEFLG_SPEED; // use sidereal position, high precision
-    // var date = {year: 1978, month: 10, day: 14, hour: 2};
-    var dateObject = new Date(Date.now());
-    var date = {year: 0, month: 0, day: 0, hour: 0};
-    date["year"] = dateObject.getFullYear();
-    date["month"] = (dateObject.getMonth() + 1); // add one because the object counts from zero and swisseph counts from one
-    date["day"] = dateObject.getDay();
-    date["hour"] = (dateObject.getHours() + (dateObject.getMinutes() / 60) + (dateObject.getSeconds() / 3600));
-    const planetaryFlags = {"sun": swisseph.SE_SUN, "moon": swisseph.SE_MOON, "mars": swisseph.SE_MARS, "mercury": swisseph.SE_MERCURY, "jupiter": swisseph.SE_JUPITER, "venus": swisseph.SE_VENUS, "saturn": swisseph.SE_SATURN, "uranus": swisseph.SE_URANUS, "rahu": swisseph.SE_TRUE_NODE, "ketu": swisseph.SE_TRUE_NODE};
-    // Julian day
-    swisseph.swe_julday (date.year, date.month, date.day, date.hour, swisseph.SE_GREG_CAL, function (julday_ut) {
-        //nresponseString += ('Julian UT day for date:' + julday_ut);
-        localPlanetaryPositions = planetaryPositions[0];
-        for (const planet in localPlanetaryPositions) {
-            // calculate planetary positions
-            swisseph.swe_calc_ut (julday_ut, planetaryFlags[planet], flag, function (body) {
-                assert (!body.error, body.error);
-                if (planet === "ketu") { // place ketu 180 degrees apart from rahu
-                    body.longitude += 180;
-                    if (body.longitude > 360) { // wrap around circle if necessary
-                        body.longitude -= 360;
+    var dateTimeString = "";
+    fetch('http://worldtimeapi.org/api/timezone/Etc/UTC', {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded', // this does not trigger a CORS preflight, works with AWS
+        },
+      })
+    .then(res => res.json())
+    .then(json => {
+        dateTimeString = new String(json["datetime"]);
+        console.log(dateTimeString);
+    })
+    .then(result => {
+        console.log('Success:', result);
+        var date = {year: 0, month: 0, day: 0, hour: 0};
+        const dateItems=dateTimeString.split("-");
+        date["year"] = parseInt(dateItems[0]);
+        date["month"] = parseInt(dateItems[1]); 
+        date["day"] = parseInt(dateItems[2]); 
+        var dateHours = (dateItems[2].slice((dateItems[2].indexOf("T") + 1), (dateItems[2].indexOf(".") - 1))); // read the hours between the T and the . non-inclusive
+        var dateHourItems = dateHours.split(":"); // split into strings for hours, minutes, seconds
+        date["hour"] = (parseInt(dateHourItems[0]) + (parseInt(dateHourItems[1]) / 60) + (parseInt(dateHourItems[2]) / 3600));
+        const planetaryFlags = {"sun": swisseph.SE_SUN, "moon": swisseph.SE_MOON, "mars": swisseph.SE_MARS, "mercury": swisseph.SE_MERCURY, "jupiter": swisseph.SE_JUPITER, "venus": swisseph.SE_VENUS, "saturn": swisseph.SE_SATURN, "uranus": swisseph.SE_URANUS, "rahu": swisseph.SE_TRUE_NODE, "ketu": swisseph.SE_TRUE_NODE};
+        // Julian day
+        swisseph.swe_julday (date.year, date.month, date.day, date.hour, swisseph.SE_GREG_CAL, function (julday_ut) {
+            localPlanetaryPositions = planetaryPositions[0];
+            for (const planet in localPlanetaryPositions) {
+                // calculate planetary positions
+                swisseph.swe_calc_ut (julday_ut, planetaryFlags[planet], flag, function (body) {
+                    assert (!body.error, body.error);
+                    if (planet === "ketu") { // place ketu 180 degrees apart from rahu
+                        body.longitude += 180;
+                        if (body.longitude > 360) { // wrap around circle if necessary
+                            body.longitude -= 360;
+                        }
                     }
-                }
-                // assuming parseInt returns only integer part without rounding
-                localPlanetaryPositions[planet] = parseInt((body.longitude / 30)); // should assign position to planet
-            });
-        }
-    });
-    // four lines below from StackOverflow to allow CORS
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    res.json(localPlanetaryPositions);
+                    // assuming parseInt returns only integer part without rounding
+                    localPlanetaryPositions[planet] = parseInt((body.longitude / 30)); // should assign position to planet
+                });
+            }
+        });
+        // four lines below from StackOverflow to allow CORS
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Credentials', true)
+        res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        res.json(localPlanetaryPositions);
+      })
+    .catch(error => {
+        console.error('Error:', error);
+        res.send("API error");
+      });
 });
 
 router.get('/test', (req, res) => {
