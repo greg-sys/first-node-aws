2a3f5fa1f99f6daf6e251c7a6a29f60b9f5162e4
Greg <greg@Gregs-MacBook-Pro.local>
2021-03-14 12:01:13 -0400

    added allow-origin to error handling

--- astroapi.js
+++ astroapi.js
@@ -170,7 +170,12 @@ router.get('/siderealPlanets', (req, res) => {
       })
     .catch(error => {
         console.error('Error:', error);
-        res.send("API error");
+        // four lines below from StackOverflow to allow CORS
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Credentials', true)
+        res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        res.send("API error: " + error.toString());
       });
 });
 
